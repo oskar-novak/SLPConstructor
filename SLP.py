49d4299db19b78beb28bb8854399c2814052ea7b
oskar-novak <138525379+oskar-novak@users.noreply.github.com>
2025-06-16 20:47:41 -0700

    Create SLP.py

---
+++ SLP.py
@@ -0,0 +1,33 @@
+def SLP(H,Lift_Size):
+    #find Generator Matrix
+
+    G=findGenerator(H,Lift_Size)
+
+    GF = galois.GF(2)
+
+    p1=galois.Poly([1],field=GF)
+    p0=galois.Poly([0],field=GF)
+
+    #construct Code
+    baseIdentity=np.empty((H.shape[1],H.shape[1]),dtype=object)
+
+    for i in range(H.shape[1]):
+        for j in range(H.shape[1]):
+            if i==j:
+                baseIdentity[i,j]=p1
+            else:
+                baseIdentity[i,j]=p0
+    
+    
+    Hx=lift(np.kron(H,G),Lift_Size)
+    Hz=lift(np.kron(G,H),Lift_Size)
+
+    Gx=lift(np.kron(H,baseIdentity),Lift_Size)
+    Gz=lift(np.kron(baseIdentity,H),Lift_Size)
+    
+    Lx=lift(np.kron(baseIdentity,G),Lift_Size)
+    Lz=lift(np.kron(G,baseIdentity),Lift_Size)
+
+    return Hx, Hz, Gx, Gz, Lx, Lz
+
+    
